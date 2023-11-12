dfd37cfa38a60448b0199c4e7c740ae3ee103318
HubulovaVI <valeriahb@mail.ru>
2023-11-12 15:11:40 +0500

    Task4

---
+++ Tyuiu.HubulovaVI.Sprint4.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.HubulovaVI.Sprint4.Task4.V1.Lib
+{
+    public class DataService : ISprint4Task4V1
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int result = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        result += matrix[i, j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
